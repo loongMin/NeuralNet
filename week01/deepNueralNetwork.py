d5dd27615c1096fc558e1730f90cbc6b31e55796
longmin <loongm@foxmail.com>
2021-11-27 00:35:43 +0800

    it looks like there is no bugs, but the gradients gradients occur cased by sigmoid_(z) = 1 as z is very big or small

--- week01/deepNueralNetwork.py
+++ week01/deepNueralNetwork.py
@@ -1,4 +1,4 @@
-import numpy as np;
+import numpy as np
 import matplotlib.pyplot as plt
 import re
 import pandas as pd
@@ -47,14 +47,9 @@ layer model:
         #learning rate: a
         #activacation: g
         #number of examples: m
-        #units: n
-        #number of layer: x
+        #units: n, nx = n_0
+        #number of layer: l
         #lose function: L
-'''
-
-'''
-
-
 '''
 
 
@@ -66,21 +61,20 @@ class NeuralNet:
     W_list = []
     b_list = []
 
-    loseFunction = ""
+    loseFunction = ""   # lose function type
+    y = []              # target y
 
     # hyperparameters
-    x = 0
-    n_list = []
-    m = 16
-    a = 0.03
-    itr = 128
+    l = 0           # number of layers
+    n_list = []     # number of units in every layer
+    m = 16          # m exmples in training
+    a = 0.03        # learning rate
+    itr = 128       # times of iteration
 
     # train state record
-    lose_list = []
-    y = []
+    lose_list = []  # lose of mean in every iteration
 
-
-    # activation function and its derivative
+    # register the activation function and its derivative to dictionary actGdic
     def sigmoid(z):
         return 1 / (1 + np.exp(-z))
 
@@ -107,7 +101,7 @@ class NeuralNet:
                 "leakyRelu": leakyRelu,
                 "leakyRelu_": leakyRelu_}
 
-    # lose function
+    # register lose function and its derivative to dictionary loseLdic
     def L_binaryClassfication(a, y):
         return -(y * np.log(a) + (1 - y) * np.log(1 - a))
 
@@ -115,27 +109,29 @@ class NeuralNet:
         return -(y / a + (1 - y) / (1 - a))
 
     def L_regression(a, y):
-        return np.sqrt(a - y)
+        return (a - y)**2
 
     def L_regression_(a, y):
-        return 2 * np.abs(a - y)
+        return 2 * (a - y)
 
     loseLdic = {"L_b": L_binaryClassfication,
                 "L_b_": L_binaryClassfication_,
                 "L_r": L_regression,
                 "L_r_": L_regression_}
 
-    def __init__(self,  m=16):
-        self.m = m
-        self.x = -1
+    #
+    def __init__(self):
+        self.l = 0
 
-    def creat_neural_network(self, n_0):
-        self.n_list.append(int(n_0))
-        self.x += 1
+    def load_data_piece(self, A_0, y):
+        self.n_list.append(A_0.shape[0])
+        self.m = A_0.shape[1]
+        self.y = y.tolist()
 
+    def creat_neural_network(self):
         self.G_list.append("")
         self.Z_list.append(np.array([]))
-        self.A_list.append(self.a_random_array(n_0, self.m))
+        self.A_list.append(self.a_random_array(self.n_list[0], self.m))
         self.W_list.append(np.array([]))
         self.b_list.append(np.array([]))
 
@@ -148,16 +144,15 @@ class NeuralNet:
             if match:
                 n_l = int(match.group(2))
                 self.n_list.append(n_l)
-                self.x += 1
+                self.l += 1
 
                 self.G_list.append(match.group(1))
                 self.Z_list.append(self.a_random_array(n_l, self.m))
                 self.A_list.append(self.a_random_array(n_l, self.m))
-                self.W_list.append(self.a_random_array(n_l, self.n_list[self.x - 1], 0.001))
+                self.W_list.append(self.a_random_array(n_l, self.n_list[self.l - 1], 0.001))
                 self.b_list.append(self.a_random_array(n_l, 1, 0.001))
             else:
                 print("pattern inputted is wrong!, re-input for this network layer.")
-                continue
 
         #
         pattern_lose = re.compile(r"(L_b|L_r)")
@@ -174,7 +169,7 @@ class NeuralNet:
         return np.random.rand(row, col) * mul
 
     def show_all_parameters_pattern(self):
-        for i in range(0, self.x+1):
+        for i in range(0, self.l + 1):
             print("======================================================================")
             print("The ", i, "th", "layer with ", self.n_list[i], "units as below:")
             Z = np.array(self.Z_list[i])
@@ -190,37 +185,22 @@ class NeuralNet:
             # print(W)
             print("b", b.shape)
             # print(b)
+        print("======================================================================")
+        print("y:", np.array(self.y).shape)
 
-    #
+    # training part
     def piece_train_network(self, itr, a):
         for i in range(0, itr):
             self.forward()
             lose_itr = self.lose()
             self.lose_list.append(lose_itr.sum()/self.m)
+
             self.backward(a, lose_itr)
             print("The ", i, "th propagation!")
 
-    def load_data_piece(self):
-        A_0 = np.array([
-            [2, 4, 2, 3, 4, 6, 3, 6, 7, 10],
-            [2, 4, 2, 3, 4, 6, 3, 6, 7, 10],
-            [2, 4, 2, 3, 4, 6, 3, 6, 7, 10],
-            [2, 4, 2, 3, 4, 6, 3, 6, 7, 10],
-            [2, 4, 2, 3, 4, 6, 3, 6, 7, 10]
-        ])
-        y = np.array([13, 21, 4, 6, 2, 4, 7, 2, 6, 6])
-
-        if A_0.shape == (self.n_list[0], self.m):
-            self.A_list[0] = A_0.tolist()
-            self.y = y.tolist()
-        else:
-            print("loading data format is wrong!")
-        return
-
     def forward(self):
         A_front = np.array(self.A_list[0])
-        for i in range(1, self.x+1):
-            self.propagaton_position = i;
+        for i in range(1, self.l + 1):
             W = np.array(self.W_list[i])
             b = np.array(self.b_list[i])
 
@@ -235,63 +215,65 @@ class NeuralNet:
             A_front = A
 
     def lose(self):
-        return np.array(list(map(self.loseLdic[self.loseFunction],
-                                 np.array(self.A_list[self.x]).flatten('C'),
-                                 np.array(self.y).flatten('C')))
-                        ).reshape(self.n_list[self.x], -1)
+        y_hat = np.array(self.A_list[self.l])
+        y = np.array(self.y)
+        if y_hat.shape == y.shape:
+            return np.array(list(map(self.loseLdic[self.loseFunction],
+                                 y_hat.flatten('C'),
+                                 y.flatten('C')))
+                            ).reshape(self.n_list[self.l], -1)
+        else:
+            print("the last layer did not pattern the format of y")
+            exit(-1)
 
     def backward(self, a, lose):
         dA = lose * np.array(list(map(self.loseLdic[self.loseFunction+"_"],
-                                      np.array(self.A_list[self.x]).flatten('C'),
-                                      self.y))
-                             ).reshape(self.n_list[self.x], -1)
-        for i in range(self.x, 0, -1):
-            self.propagaton_position = i
+                                      np.array(self.A_list[self.l]).flatten('C'),
+                                      np.array(self.y).flatten('C')))
+                             ).reshape(self.n_list[self.l], -1)
+        for i in range(self.l, 0, -1):
             Z = np.array(self.Z_list[i])
-            A = np.array(self.A_list[i])
             W = np.array(self.W_list[i])
             b = np.array(self.b_list[i])
             A_1 = np.array(self.A_list[i-1])
 
-            # backawrd propagation
+
+
+            # backward propagation
             dZ = dA * np.array(list(map(self.actiGdic[self.G_list[i]+"_"], Z.flatten('C')))
                                ).reshape(self.n_list[i], -1)
             dW = np.dot(dZ, A_1.T) / self.m
             db = np.sum(dZ, axis=1, keepdims=True) / self.m
             dA = np.dot(W.T, dZ)
 
+            if i == 1:
+                print("---------------------------------------------------------------")
+                print(dW)
             # refresh the W and b
             W = W - a * dW
             b = b - a * db
-            self.W = W.tolist()
-            self.b = b.tolist()
+            self.W_list[i] = W.tolist()
+            self.b_list[i] = b.tolist()
 
     def show_lose(self):
         plt.plot(self.lose_list)
-
-
-
-
-
+        plt.show()
 
 
 
 
 if __name__ == '__main__':
-    neuralNet = NeuralNet(10)
-    neuralNet.creat_neural_network(5)
-    neuralNet.show_all_parameters_pattern()
+    neuralNet = NeuralNet()
 
-    neuralNet.load_data_piece()
-    neuralNet.piece_train_network(1000, 0.3)
-    neuralNet.show_all_parameters_pattern()
+
+    neuralNet.load_data_piece(x, y)
+    neuralNet.creat_neural_network()
+    neuralNet.piece_train_network(10, 0.3)
     neuralNet.show_lose()
 
 
-
-
-
-
-
-
-
+    df_x = pd.read_csv("./data/house_price/test.csv")
+    df_y = pd.read_csv("./data/house_price/price.csv")
+    features = ['MSSubClass', 'LotFrontage', 'LotArea']
+    x = np.array(df_x[features].T)
+    y = np.array([np.array(df_y['SalePrice'].T).tolist()])
